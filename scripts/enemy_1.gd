b452ed2d7d889ae648858fc968bb30af3d10aac4
BKZeigler <BKZeigler@csu.fullerton.edu>
2025-04-13 11:10:03 -0700

    Finished enemy behavior and key bindings

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -4,11 +4,20 @@ const SPEED = 50
 
 var direction = 1
 
+@onready var right_check = $Right_Check
+@onready var left_check = $Left_Check
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x += direction * SPEED * delta
+	if right_check.is_colliding():
+		position.x += direction * (SPEED * 1.2) * delta
+	if left_check.is_colliding():
+		position.x += -direction * (SPEED * 1.2) * delta
+	else:
+		position.x += direction * SPEED * delta
 	
 
 func _on_timer_timeout() -> void:
 	direction = -direction
-	$AnimatedSprite2D.flip_h = direction < 0	
+	$AnimatedSprite2D.flip_h = direction < 0
+	
