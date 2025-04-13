279d617c8041b749363eccc9092f37a6869cd130
nrajesh2305 <rajeshnithin2305@csu.fullerton.edu>
2025-04-12 23:01:42 -0700

    Finished Labeling Controls for Tutorial Level.

--- scripts/planet_1.gd
+++ scripts/planet_1.gd
@@ -1,14 +1,9 @@
 extends Node2D
 
-signal planet_changed(planet_name)
-
-@export () var planet_name = "planet_one"
-
 @onready var test_ray = $RayCast2D
-
 
 func _physics_process(delta):
 	if test_ray.is_colliding():
 		var collider = test_ray.get_collider()
 		if collider.name == "Player":
-			emit_signal("planet_changed", planet_name)
+			collider.get_hit(10)
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,19 +4,11 @@ var max_oxygen := 100
 var current_oxygen := 100
 
 @onready var oxygen_bar := $OxygenBar
-@onready var oxygen_loss = $Timer
 
 # OXYGEN BAR FUNCTIONALITY
 
 func _ready():
 	update_oxygen_bar()
-	oxygen_loss.wait_time = 1.0
-	oxygen_loss.start()
-	
-func _on_timer_timeout():
-	if current_oxygen > 0:
-		current_oxygen -= 1
-		 # Replace with function body.
 	
 func get_hit(damage: int):
 	current_oxygen = max(current_oxygen - damage, 0)
@@ -40,8 +32,8 @@ func death():
 
 #MOVEMENT AND GRAVITY FUNCTIONALITY
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
