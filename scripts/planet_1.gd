ebd36a5401adac62b3f26f264473a3def8f2eb62
BKZeigler <BKZeigler@csu.fullerton.edu>
2025-04-12 22:16:59 -0700

    Oxygen Bar depletion

--- scripts/planet_1.gd
+++ scripts/planet_1.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+signal planet_changed(planet_name)
+
+@export () var planet_name = "planet_one"
+
 @onready var test_ray = $RayCast2D
+
 
 func _physics_process(delta):
 	if test_ray.is_colliding():
 		var collider = test_ray.get_collider()
 		if collider.name == "Player":
-			collider.get_hit(10)
+			emit_signal("planet_changed", planet_name)
