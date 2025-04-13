ec540d45cc88f4ea4d383ef47b0874d236e87b1f
AC350144 <AC350144@gmail.com>
2025-04-13 14:18:05 -0700

    options menu functions

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,10 +8,11 @@ func _on_play_pressed() -> void:
 	emit_signal("planet_changed", planet_name + "_play") #options will be + "_options"
 	#get_tree().change_scene_to_file("game_scene_here")
 
-
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()	
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
